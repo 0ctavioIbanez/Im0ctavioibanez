941ddc0ae15c60eeb5afbff48d47b13d3da159f2
Octavio Ibanez <octaviogd@live.com.mx>
2020-09-06 00:43:02 -0500

    Footer added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,26 +5,26 @@ const Footer = () => {
     return (
         <footer>
             <div className="container flex">
-                <fieldset>
-                    <legend>¡Conversemos!</legend>
-                    <form>
-                        <div className="div-form">
-                            <label>Nombre</label>
-                            <input type="text" />
-                        </div>
-                        <div className="div-form">
-                            <label>Contacto:</label>
-                            <input type="text" />
-                        </div>
-                        <div className="div-form">
-                            <label>Asunto</label>
-                            <textarea></textarea>
-                        </div>
-                        <div className="btn-container">
-                            <button type="submit" className="button btn-secundary">Enviar</button>
-                        </div>
-                    </form>
-                </fieldset>
+                <div className="test-footer">
+                    <h2 className="titulo white"><i class="fas fa-mug-hot"></i> Charlemos</h2>
+                </div>
+                <form>
+                    <div className="div-form">
+                        <label>Nombre</label>
+                        <input type="text" />
+                    </div>
+                    <div className="div-form">
+                        <label>Contacto:</label>
+                        <input type="text" />
+                    </div>
+                    <div className="div-form">
+                        <label>Asunto</label>
+                        <textarea></textarea>
+                    </div>
+                    <div className="btn-container flex">
+                        <button type="submit" className="button btn-primary">Enviar</button>
+                    </div>
+                </form>
             </div>
         </footer>
     );
